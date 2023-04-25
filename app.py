f1bb9f8d34a10e4a421fb138240ecd0f26a48e87
Talha-Altair <cooltalha27@gmail.com>
2023-04-26 01:54:05 +0530

    Updates for 2023-04-26 01:54:05

--- app.py
+++ app.py
@@ -7,5 +7,18 @@ def index():
 
     return render_template('index.html')
 
+@app.route('/teachers', methods=['GET', 'POST'])
+def teachers():
+
+    if request.method == 'POST':
+
+        payload = dict(request.form)
+
+        
+
+        return render_template('teachers.html', alert_msg='Teacher added successfully!')
+
+    return render_template('teachers.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
