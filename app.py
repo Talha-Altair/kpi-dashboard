4c41ee1f5b7e2859b1700a35a06fcba2164a7aa7
Talha-Altair <cooltalha27@gmail.com>
2023-04-26 02:46:19 +0530

    Updates for 2023-04-26 02:46:19

--- app.py
+++ app.py
@@ -1,24 +1,49 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
+from settings import HOST, PORT, BACKEND_URL
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
+    return render_template("index.html")
 
-    return render_template('index.html')
 
-@app.route('/teachers', methods=['GET', 'POST'])
+@app.route("/teachers", methods=["GET", "POST"])
 def teachers():
-
-    if request.method == 'POST':
+    if request.method == "POST":
 
         payload = dict(request.form)
 
-        
+        payload['subject_codes'] = []
 
-        return render_template('teachers.html', alert_msg='Teacher added successfully!')
+        res = requests.post(BACKEND_URL + "/teaching/add", json=payload)
 
-    return render_template('teachers.html')
+        if res.status_code != 200:
 
-if __name__ == '__main__':
-    app.run(debug=True)
+            return render_template("teachers.html", alert_msg="Something went wrong!")
+
+        return render_template("teachers.html", alert_msg="Teacher added successfully!")
+
+    return render_template("teachers.html")
+
+@app.route("/subjects/add", methods=["GET", "POST"])
+def subjectsadd():
+
+    if request.method == "POST":
+
+        payload = dict(request.form)
+
+        res = requests.post(BACKEND_URL + "/teaching/addsubjects", json=payload)
+
+        if res.status_code != 200:
+
+            return render_template("addsubjects.html", alert_msg="Something went wrong!")
+
+        return render_template("addsubjects.html", alert_msg="Subject added successfully!")
+
+    return render_template("addsubjects.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host=HOST, port=PORT)
--- settings.py
+++ settings.py
@@ -4,5 +4,5 @@ import os
 load_dotenv()
 
 HOST = os.getenv('HOST', '0.0.0.0')
-PORT = os.getenv('PORT', '9000')
+PORT = os.getenv('PORT', '3000')
 BACKEND_URL = os.getenv('BACKEND_URL')
